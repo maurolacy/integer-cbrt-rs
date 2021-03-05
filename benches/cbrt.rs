7477cd8c532e1e971f4b2763607c9f506774de39
Mauro Lacy <mauro@lacy.com.es>
2021-03-05 19:57:06 +0100

    Add u128 benchmarks

--- benches/cbrt.rs
+++ benches/cbrt.rs
@@ -19,7 +19,7 @@ fn icbrt_via_f64(n: u64) -> u64 {
 }
 
 #[bench]
-fn icbrt_small(b: &mut Bencher) {
+fn icbrt_u64_small(b: &mut Bencher) {
     let small = 511u64;
     b.iter(|| {
         let n = black_box(small);
@@ -28,7 +28,7 @@ fn icbrt_small(b: &mut Bencher) {
 }
 
 #[bench]
-fn icbrt_med(b: &mut Bencher) {
+fn icbrt_u64_med(b: &mut Bencher) {
     let med = 1_000_000_000_000_000u64; // 10^15
     b.iter(|| {
         let n = black_box(med);
@@ -37,11 +37,38 @@ fn icbrt_med(b: &mut Bencher) {
 }
 
 #[bench]
-fn icbrt_large(b: &mut Bencher) {
+fn icbrt_u64_large(b: &mut Bencher) {
     let large = u64::MAX;
     b.iter(|| {
         let n = black_box(large);
         assert_eq!(n.integer_cbrt_checked(), Some(2642245));
+    })
+}
+
+#[bench]
+fn icbrt_u128_small(b: &mut Bencher) {
+    let small = 511u128;
+    b.iter(|| {
+        let n = black_box(small);
+        assert_eq!(n.integer_cbrt_checked(), Some(7));
+    })
+}
+
+#[bench]
+fn icbrt_u128_med(b: &mut Bencher) {
+    let med = 1_000_000_000_000_000u128; // 10^15
+    b.iter(|| {
+        let n = black_box(med);
+        assert_eq!(n.integer_cbrt_checked(), Some(100_000)); // 10^5
+    })
+}
+
+#[bench]
+fn icbrt_u128_large(b: &mut Bencher) {
+    let large = u128::MAX;
+    b.iter(|| {
+        let n = black_box(large);
+        assert_eq!(n.integer_cbrt_checked(), Some(6981463658331));
     })
 }
 
